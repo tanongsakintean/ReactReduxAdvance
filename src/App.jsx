02bcacaf4f63e82f6801dfeb54344751c881d147
tanongsak intean <tanongsakintean.5333@gmail.com>
2023-01-05 22:09:08 +0700

    update advane

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,49 @@
+import { useGetAllAttractionQuery } from "./services/attraction";
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "name",
+    headerName: "name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "detail",
+    headerName: "Detail",
+    width: 1400,
+    editable: true,
+  },
+];
+
 function App() {
-  return <div className="App"></div>;
+  const { data, error, isLoading } = useGetAllAttractionQuery();
+  console.log(data);
+  return (
+    <div className="p-10">
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <div className="">
+          <Box sx={{ height: 400, width: "100%" }}>
+            <DataGrid
+              rows={data}
+              columns={columns}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+              checkboxSelection
+              disableSelectionOnClick
+              experimentalFeatures={{ newEditingApi: true }}
+            />
+          </Box>
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
 export default App;
